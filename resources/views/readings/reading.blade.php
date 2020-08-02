5dd7b1d881bedc5a8174672880af570f3f152d88
Marian-Bogdan <bgdmarian@gmail.com>
2020-08-02 12:52:32 +0300

    --added button readings on home page and added class on table readings

--- resources/views/readings/reading.blade.php
+++ resources/views/readings/reading.blade.php
@@ -1,24 +1,29 @@
 @extends('layouts.app')
 
 @section('content')
- 
- <table class="table" >
 
- <th> ID    </th>
- <th> Value </th>
- <th> Name  </th>
- <th> Location </th>
- <th> Name </th>
- <th> Created At </th>  
-    @foreach ($logs as $log)
-       <tr>            
-            <td>{{ $log->id }}</td> 
-            <td> {{ $log->value }} </td> 
-            <td> {{$log->deviceRelation->name}} </td> 
-            <td> {{$log->deviceRelation->location}} </td> 
-            <td> {{$log->physicalPropertyRelation->name}} </td>
-            <td> {{$log->created_at}} </td>
-       </tr>
-     @endforeach
- </table>
+ <div class="container">
+ <h6>Todays Readings</h6>   
+   <a href="/home" class="btn btn-primary btn-sm"> Back </a>       
+      <div class="container"> <br>
+         <table class="table table-hover table-sm" >
+            <th> ID           </th>
+            <th> Device Name  </th>
+            <th> Location     </th>
+            <th> Property     </th>
+            <th> Value        </th>
+            <th> Created At   </th>  
+               @foreach ($logs as $log)
+                  <tr>            
+                        <td>{{ $log->id }}</td> 
+                        <td> {{$log->deviceRelation->name}} </td> 
+                        <td> {{$log->deviceRelation->location}} </td> 
+                        <td> {{$log->physicalPropertyRelation->name}} </td>
+                        <td> {{ $log->value }} </td> 
+                        <td> {{$log->created_at}} </td>
+                  </tr>
+               @endforeach
+         </table>
+      </div>
+</div>
 @endsection
